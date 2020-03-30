dc2448d45c63c71f3904963e523f9be7620ca433
satyam9753 <satyam.anand9753@gmail.com>
2020-03-31 02:39:54 +0530

    Done------> more features to be added

---
+++ font/Readme First.txt
@@ -0,0 +1,40 @@
+Thanks for downloading!
+===============================================================
+This DEMO FONT is for PERSONAL USE ONLY!
+===============================================================
+
+By installing or using this font, you are agree to the Product Usage Agreement:
+
+1. This font is ONLY for PERSONAL USE. NO COMMERCIAL USE ALLOWED!
+
+2. You are requires a license for PROMOTIONAL or COMMERCIAL use.
+
+3. CONTACT ME before any Promotional or Commercial Use!
+
+EMAIL SUPPORT:
+rismanginarwan8@gmail.com
+support@garisman.com
+
+===============================================================
+
+COMMERCIAL LICENSE:
+
+http://www.garisman.com
+
+https://creativemarket.com/Garisman?u=Garisman
+
+===============================================================
+
+Paypal account for donation : https://paypal.me/rginarwan/
+
+===============================================================
+
+NOTE:
+- If there is a problem, question, or anything about my fonts, please sent an email to: support@garisman.com
+
+
+- Share your work with this font and tag us on instagram @grsmn.id #grsmnid
+
+================
+Best Regards,
+Garisman Studio
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import pygame
+from pygame import mixer
 import random
+from math import sqrt, pow
 
 #initialize the pygame 'always there'
 pygame.init()
@@ -9,52 +11,91 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600)) # 'width' & 'height' respectively in pixels
 
 #background
-background = pygame.image.load("background1.png")
+background = pygame.image.load("images/background1.png")
+
+#music
+mixer.music.load('audio/backgroundMusic.wav')
+mixer.music.play(-1)
+
 
 #TITLE AND ICON
 pygame.display.set_caption("Invasion")
-logo = pygame.image.load('pirate.png')
+logo = pygame.image.load('images/pirate.png')
 pygame.display.set_icon(logo)
 
 #PLAYER
-player_img = pygame.image.load('jet.png')
+player_img = pygame.image.load('images/jet.png')
 playerX = 370
 playerY = 450
 playerX_change = 0.3
 
+#player_score
+playerScore = 0
+fontScore = pygame.font.Font('font/GRNORCH.ttf', 40)
+fontGameOver = pygame.font.Font('font/Fidalga-Regular.ttf', 64)
+textX = 10
+textY =10
+
 #ENEMY
-enemy_img = pygame.image.load('alien(1).png')
-enemyX = random.randint(0,800)
-enemyY = random.randint(50,150)
-enemyX_change = 1
-enemyY_change = 32
+numberEnemies = 8
+enemy_img = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+
+for i in range(numberEnemies):
+    key = random.randint(1,2)
+    enemy_img.append(pygame.image.load('images/alien'+str(key)+'.png'))
+    #enemy_img.append(pygame.image.load('alien1.png'))
+    enemyX.append(random.randint(0,730))
+    enemyY.append(random.randint(50,150))
+    enemyX_change.append(1)
+    enemyY_change.append(32)
 
 
 #BULLET
 #'hidden': bullet not visible
 #'visible' : bullet is moving
-bullet_img = pygame.image.load('bullet.png')
+bullet_img = pygame.image.load('images/bullet.png')
 bulletX = 0
 bulletY = 480
 bulletX_change = 0
-bulletY_change = 3
+bulletY_change = 3.5
 bullet_state = "hidden"
+
+def showScore(x, y):
+    score = fontScore.render("Your Score:  " + str(playerScore), True, (0,255,25))
+    screen.blit(score, (x,y))
 
 
 def player(x, y):
     screen.blit(player_img, (x,y)) # 'blit' means to draw
 
-def enemy(x, y):
-    screen.blit(enemy_img, (x,y))
+def enemy(x, y, i):
+    screen.blit(enemy_img[i], (x,y))
 
 def fire_bullet(x, y):
     global bullet_state
     bullet_state = "visible"
-    screen.blit(bullet_img, (x+16, y+10))
+    screen.blit(bullet_img, (x+16, y-40))
+
+
+def collision(enemyX, enemyY, bulletX, bulletY):
+    
+    distance_between_enemy_bullet = sqrt(pow((enemyX - bulletX), 2) + pow((enemyY - bulletY), 2))
+    if (distance_between_enemy_bullet < 25):
+        return True
+    else:
+        return False
+
+
+def gameOver():
+    over = fontGameOver.render("GAME OVER :(" + str(playerScore), True, (0,255,25))
+    screen.blit(over, (200,250))
 
 # GAME LOOP
 flag = True
-
 while flag:
 
     screen.fill((75,79,83)) #(R, G, B)
@@ -67,14 +108,15 @@ while flag:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -1.5
+                playerX_change = -2.5
 
             if event.key == pygame.K_RIGHT:
-                playerX_change = 1.5
+                playerX_change = 2.5
             
             if event.key == pygame.K_SPACE:
-                bulletX = playerX
-                fire_bullet(bulletX, bulletY)
+                if bullet_state == "hidden":
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -87,15 +129,37 @@ while flag:
     elif playerX >= 736:
         playerX = 736
 
-    #ENEMY BOUNDARY CHECK
-    enemyX += enemyX_change
 
-    if enemyX < 0:
-        enemyX_change = 1
-        enemyY += enemyY_change
-    elif enemyX >= 736:
-        enemyX_change = -1
-        enemyY += enemyY_change
+    #ENEMY BOUNDARY CHECK
+    for i in range(numberEnemies):
+
+        #GAME---OVER
+        if enemyY[i] > 430:
+            for j in range(1,numberEnemies):
+                enemyY[j] == 5000 ##That's all folks :)
+            gameOver()
+            break
+
+        enemyX[i] += enemyX_change[i]
+        
+        if enemyX[i] < 0:
+            enemyX_change[i] = 1 + (playerScore/50)
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -(1 + (playerScore/50))
+            enemyY[i] += enemyY_change[i]
+
+            #collision
+        collide = collision(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collide == True:
+            enemyX[i] = random.randint(0,730)
+            enemyY[i] = random.randint(50,150)
+            bulletY = 480
+            bullet_state = "hidden"
+            playerScore += 1
+        
+        enemy(enemyX[i], enemyY[i], i)
+
 
     #bullet movement
     if bulletY <= 0:
@@ -106,8 +170,7 @@ while flag:
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
-
+    
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
+    showScore(textX, textY)
     pygame.display.update()  #always there
-
